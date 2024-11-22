4b7fb8b3d91e6a250f0e9cb9ef9c8edb3d063021
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-11-22 11:51:53 -0300

    nome atualizado

--- src/app/membros/Cards.tsx
+++ src/app/membros/Cards.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Cards() {
   const participants = [
     {
-      name: "Cauan Aranega Schot Passos",
+      name: "Cauan Aranega S Passos",
       rm: "RM555466",
       imageSrc: "/images/members/cauan.png",
       github: "https://github.com/cashot01",
